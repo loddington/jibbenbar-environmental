9585115650fc6f164194d7c10b8eb8175e5da614
Duncan <49673638+loddington@users.noreply.github.com>
2024-11-06 16:11:49 +1100

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# WiFi credentials
+WIFI_SSID = "XXXXXXXXX"
+WIFI_PASSWORD = "XXXXXXXXXXXXX"
+
+# Server URL for the PUT request
+SERVER_URL = "http://API-ENDPOINT-SERVER:5000/sensors/bucket_tips/increment"
+
+
+# Just so I can see in the logs that the machine is alive of flapping.
+SERVER_URL_RESTART ="http://severn-data.loddington.com:5000/sensors/restart/increment" 
+
+# Hostname for DHCP identification
+HOSTNAME = "ESP-Battery-Room"
+LORA_ID = "XXXXXXXXX"
+
+LORA_FREQ = "AU915"
+
